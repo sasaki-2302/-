8efb2b7e85e70c6c65c97e1e771a53b1d2d4cced
sasaki-2302 <124408816+sasaki-2302@users.noreply.github.com>
2023-04-02 17:41:47 +0900

    cretae_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,33 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :admin do
+    get 'members/index'
+    get 'members/show'
+    get 'members/edit'
+  end
+  namespace :admin do
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/edit'
+  end
+  namespace :public do
+    get 'chats/show'
+  end
+  namespace :public do
+    get 'relationships/followings'
+    get 'relationships/followers'
+  end
+  namespace :public do
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/edit'
+  end
   namespace :admin do
   end
 
